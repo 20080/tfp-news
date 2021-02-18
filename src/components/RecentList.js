b11bb9c6ad68c3678fefe0f21056cc6eda78b7dc
20080 <surajkumar2091@gmail.com>
2021-02-18 18:04:59 +0530

    Test Deployment responsive partial Main Page Content added-- front end styling added header.js width adjustment causing flicker bug + little touch

--- src/components/RecentList.js
+++ src/components/RecentList.js
@@ -1,7 +1,11 @@
 const RecentList = ()=>{
     return(
         <div className={'recentStory'}>
-            <h2>Recent Articles</h2>
+            <div id={'recentList-Header'}>
+                <h1>Recent</h1>
+                <h2> Articles</h2>
+            </div>
+
         <ul>
             <li><a>Item 1 Extra Text</a></li>
             <li><a>Item 2 Extra Text</a></li>
