ff839885f12baf881b004db5ff82c61bbb225bff
20080 <surajkumar2091@gmail.com>
2021-02-18 18:39:19 +0530

    Test Deployment responsive partial Main Page Content added-- front end styling added header.js width adjustment causing flicker bug + little touch +Added Router

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Header from "./components/Header";
 import {useState} from 'react'
 import Storybox from "./components/Storybox";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, useLocation } from "react-router-dom"
 
 function App() {
 
 
     const [newsHead,setNewsHead] = useState('TFP news')
   return (
+      <Router>
     <div className={'App'}>
 <Header newsHead={newsHead}/>
 <Storybox/>
 <Footer/>
     </div>
+      </Router>
   );
 }
 
